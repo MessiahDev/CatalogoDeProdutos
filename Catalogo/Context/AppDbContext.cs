f725c3b811eb1df01d21230cebdf7d0d1c395313
Alex Messias <alexmessias_18@hotmail.com>
2023-09-01 14:23:53 -0300

    Adicionar arquivos de projeto.

---
+++ Catalogo/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Catalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
